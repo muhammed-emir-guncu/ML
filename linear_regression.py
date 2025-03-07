30bc7207c15db6659eb30e4fe331ba7e082154ed
Muhammed Emir GÜNCÜ <muhammedemirguncu@gmail.com>
2025-03-07 07:48:50 +0300

    linear regresion eklendi

---
+++ linear_regression.py
@@ -0,0 +1,43 @@
+"""lsls"""
+import numpy as np
+#import pandas as pd
+import matplotlib.pyplot as plt
+veri_x=[i for i in range(1,200)]
+veri_y=[i*2+5+np.random.normal(0,50) for i in veri_x]
+
+
+def kayip_fonk(m,b,p_x:list,p_y:list):
+    """kayıp"""
+    toplam_hata=0
+    assert len(p_x)==len(p_y)
+    for i in range(len(p_x)):
+        x=p_x[i]
+        y=p_y[i]
+        toplam_hata += (y - (m * x + b))**2
+    return toplam_hata/float(len(p_x))
+
+def gradyan(m,b,p_x,p_y,l):
+    """grafyan"""
+    assert len(p_x)==len(p_y)
+    m_g=0
+    b_g=0
+    n=len(p_x)
+    for i in range(n):
+        x=p_x[i]
+        y=p_y[i]
+        m_g += -(2/n) * x *(y-(m * x + b))
+        b_g += -(2/n) * (y-(m * x + b))
+    m = m - m_g * l
+    b = b - b_g * l
+    return m, b
+m_=0
+b_=0
+l=0.0001
+epoch=100
+for i in range(epoch):
+    print(i)
+    m, b=gradyan(m_,b_,veri_x,veri_y,l)
+print(m,b)
+plt.scatter(veri_x,veri_y)
+plt.plot(list(range(0,200)),[m*i+b for i in range(0,200)])
+plt.show()
